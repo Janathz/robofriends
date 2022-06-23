15b2da67472aa740ecf88db8ca4b7e2351e85632
Janath Wijerathna <Janathz@Lakmas-MacBook-Air.local>
2022-06-24 03:25:49 +0530

    adding the array ofrobots

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
+    const { name, email, id} = props;
     return(
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src='https://robohash.org/test?size=200x200'/>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src={`https://robohash.org/${props.id}?size=200x200`}/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
