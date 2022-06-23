ecbd971f283dc31533681d5c99ba539cd2a169f4
Janath Wijerathna <Janathz@Lakmas-MacBook-Air.local>
2022-06-24 03:32:48 +0530

    modified props by cleaning

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './Card.css';
 
-const Card = (props) => {
-    const { name, email, id} = props;
+// const Card = (props) => {
+    // const { name, email, id} = props;    
+const Card = ({ name, email, id }) => {
     return(
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src={`https://robohash.org/${props.id}?size=200x200`}/>
+            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
