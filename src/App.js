dba536b1050c1b42e4794c5cf18e87fffc1cf94b
Janath Wijerathna <Janathz@Lakmas-MacBook-Air.local>
2022-06-24 11:30:36 +0530

    search change dynamic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Cardlist from "./Cardlist";
-import SearchBox from './SearchBox';
-import {robots} from './robots';
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+}
 
-const App = () => {
+onSearchChange (event){
+    console.log(event.target.value);
+}
+
+  render() {
     return (
-        <div className="tc">
+      <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox> </SearchBox>
-        <Cardlist robots={robots}/>
-        </div>
-        
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Cardlist robots={this.state.robots} />
+      </div>
     );
+  }
 }
 
 export default App;
